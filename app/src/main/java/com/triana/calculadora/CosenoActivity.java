51c1473761fd8e6e8d56ecb6479865becc1648cb
Triana-619 <145723695+Triana-619@users.noreply.github.com>
2024-03-05 20:45:19 -0500

    Montaje de pagina de resultado

--- app/src/main/java/com/triana/calculadora/CosenoActivity.java
+++ app/src/main/java/com/triana/calculadora/CosenoActivity.java
@@ -2,40 +2,49 @@ package com.triana.calculadora;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 public class CosenoActivity extends AppCompatActivity {
-    EditText cateto_adyacente, hipotenusa;
+    EditText angulocoseno;
     Button calcular_coseno;
     TextView resultado;
+
+    RadioButton radioGrados;
+    RadioButton radioRadianes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_coseno);
 
-        EditText cateto_adyacente = findViewById(R.id.txtadyacente);
-        EditText hipotenusa = findViewById(R.id.txthipotenusacoseno);
-        Button calcular_coseno = findViewById(R.id.btncalcular_coseno);
-        TextView resultado = findViewById(R.id.lblresultadocoseno);
+        angulocoseno = findViewById(R.id.txtangulocoseno);
+        calcular_coseno = findViewById(R.id.btncalcular_coseno);
+        resultado = findViewById(R.id.lblresultadocoseno);
+        radioGrados = findViewById(R.id.gradoscoseno);
+        radioRadianes = findViewById(R.id.radianescoseno);
 
         calcular_coseno.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                String adjacentString = cateto_adyacente.getText().toString();
-                String hypotenuseString = hipotenusa.getText().toString();
-                if (!adjacentString.isEmpty() && !hypotenuseString.isEmpty()) {
-                    double adjacent = Double.parseDouble(adjacentString);
-                    double hypotenuse = Double.parseDouble(hypotenuseString);
-                    double coseno = adjacent / hypotenuse;
-                    resultado.setText("Coseno: " + coseno);
+                String anguloString = angulocoseno.getText().toString();
+                if (!anguloString.isEmpty()) {
+                    double angulo = Double.parseDouble(anguloString);
+                    double radianes = radioGrados.isChecked() ? Math.toRadians(angulo) : angulo;
+                    double coseno = Math.cos(radianes);
+                    Intent intent = new Intent(CosenoActivity.this, ResultadoActivity.class);
+                    intent.putExtra("coseno", coseno); // Aquí "coseno" es la clave para el extra
+                    startActivity(intent);
                 } else {
-                    resultado.setText("Por favor, ingresa los lados del triángulo.");
+                    resultado.setText("Por favor, ingresa un ángulo.");
                 }
             }
         });
     }
 }
+
